e0a814214f471d98cbce2a99c73fde6946aecf23
pascalmlaurent <pascamlaurent@gmail.com>
2019-03-29 14:15:20 -0400

    Correct another bug and refactor code

--- Methods/src/com/tipizo/Main.java
+++ Methods/src/com/tipizo/Main.java
@@ -55,14 +55,15 @@ public class Main {
     }
 
     public static int calculateHighScorePosition(int score) {
-        if (score >= 1000 ) {
-            return 1;
-        } else if (score >= 500 && score < 1000) {
-            return 2;
-        } else if (score >= 100 && score < 500) {
-            return 3;
-        } else {
-            return 4;
-        }
+
+        int position = 4;
+        if (score >= 1000)
+            position = 1;
+        else if (score >= 500)
+            position = 2;
+        else if (score >= 100)
+            position = 3;
+
+        return position;
     }
 }
