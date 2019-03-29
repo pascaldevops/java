b801cdf62a8a11b02994a4060308059d0db32e56
pascalmlaurent <pascamlaurent@gmail.com>
2019-03-29 13:45:12 -0400

    Adding displayHighScorePosition and calculateHighScorePosition function

--- Methods/src/com/tipizo/Main.java
+++ Methods/src/com/tipizo/Main.java
@@ -15,6 +15,19 @@ public class Main {
 
         int highScore = calculateScore2(false, 300, 2, 50);
         System.out.println("Your final score was: " + highScore);
+
+        int scoreResult = calculateHighScorePosition(1500);
+        displayHighScorePosition("Pascal", scoreResult);
+
+        scoreResult = calculateHighScorePosition(900);
+        displayHighScorePosition("Scott", scoreResult);
+
+        scoreResult = calculateHighScorePosition(400);
+        displayHighScorePosition("Paul", scoreResult);
+
+        scoreResult = calculateHighScorePosition(50);
+        displayHighScorePosition("Suzie", scoreResult);
+
     }
 
     public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -35,5 +48,21 @@ public class Main {
         }
 
         return -1;
+    }
+
+    public static void displayHighScorePosition(String playerName, int position) {
+        System.out.println(playerName + " managed to get  into position " + position);
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if (score > 1000 ) {
+            return 1;
+        } else if (score > 500 && score < 1000) {
+            return 2;
+        } else if (score > 100 && score < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 }
